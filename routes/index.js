fef122bd7c1657ef27147be455d66a791ecd75ac
elilaiono <elilaiono@gmail.com>
2023-03-11 22:19:11 -0700

    added swagger, we will change connection soon

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // ADD DB COLLECTION HERE
-router.use('/', require('./'));
+router.use('/player_info', require('./player_info'));
 router.use('/', require('./swagger'));
 
 module.exports = router;
